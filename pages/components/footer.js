7f0802f46666793d90abbd384c9ff61f7c37520d
Shiv <shivg03@gmail.com>
2022-02-13 18:10:11 +0530

    day 2
    day 2

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -2,8 +2,13 @@ import styles from "../../styles/footer.module.css"
 
 export default function Row() {
   return (
-    <div>
-        <h1>hi</h1>
+    <div className={styles.bottom}>
+      <nav className={styles.foot}>
+      
+      <a href="https://www.instagram.com/shiv_gupta1/"><img src="https://cdn.discordapp.com/attachments/941091409509896283/942242694640574524/Vector.png" className={styles.img1}/></a>
+      <a href="https://www.linkedin.com/in/shivvguptaa/"><img src="https://cdn.discordapp.com/attachments/941091409509896283/942242624285310976/Vector.png" className={styles.img1}/></a>
+      <a href="mailto:shivg03@gmail.com"><img src="https://cdn.discordapp.com/attachments/941091409509896283/942242534699184128/Group.png" className={styles.img1}/></a>
+      </nav>
     </div>
   )
 }
